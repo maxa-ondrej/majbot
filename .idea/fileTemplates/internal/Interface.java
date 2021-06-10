1cb3f6ca2223c13bf794f521093db1c249cb6182
Ondřej Maxa <ondrej.maxa11@gmail.com>
2021-06-10 23:06:04 +0200

    Code style + copyright

---
+++ .idea/fileTemplates/internal/Interface.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * <p><b>Interface {@link ${NAME}}</b></p>
+ *
+ * @author ${USER}
+ * @version ${VERSION}
+ * @since ${VERSION}
+ */
+public interface ${NAME} {
+}
