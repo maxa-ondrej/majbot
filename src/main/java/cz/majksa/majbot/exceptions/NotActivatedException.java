8f5cf1494e4b3d222bd7548919170d87a972ce85
majksa <info@majksa.cz>
2021-07-06 13:38:56 +0200

    First Server and MajBot idea

---
+++ src/main/java/cz/majksa/majbot/exceptions/NotActivatedException.java
@@ -0,0 +1,35 @@
+/*
+ *  majbot - cz.majksa.majbot.exceptions.AlreadyActivatedException
+ *  Copyright (C) 2021  Majksa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package cz.majksa.majbot.exceptions;
+
+import java.io.Serial;
+
+/**
+ * <p><b>Class {@link cz.majksa.majbot.exceptions.NotActivatedException}</b></p>
+ *
+ * @author majksa
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class NotActivatedException extends IllegalStateException {
+
+    @Serial
+    private static final long serialVersionUID = 4908383142811040623L;
+
+}
